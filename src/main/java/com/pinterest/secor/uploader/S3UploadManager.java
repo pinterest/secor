b87ac386e735263a68f095a8f28bda8a7b119d71
Shawn Nguyen <shnguyen@pinterest.com>
2016-04-19 23:37:45 -0700

    removed unneeded imports; overwritten by last merge

--- src/main/java/com/pinterest/secor/uploader/S3UploadManager.java
+++ src/main/java/com/pinterest/secor/uploader/S3UploadManager.java
@@ -46,10 +46,6 @@ import com.pinterest.secor.common.LogFilePath;
 import com.pinterest.secor.common.SecorConfig;
 import com.pinterest.secor.util.FileUtil;
 
-import com.pinterest.secor.common.LogFilePath;
-import com.pinterest.secor.common.SecorConfig;
-import com.pinterest.secor.util.FileUtil;
-
 /**
  * Manages uploads to S3 using the TransferManager class from the AWS
  * SDK.
