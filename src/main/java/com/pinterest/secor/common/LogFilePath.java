a88dd58d89e387c9e487b02d688217956c217fc2
rob mccoll <rmccoll@ionicsecurity.com>
2017-01-23 16:21:05 -0500

    if no prefix/topic, don't add empty component to path

--- src/main/java/com/pinterest/secor/common/LogFilePath.java
+++ src/main/java/com/pinterest/secor/common/LogFilePath.java
@@ -146,8 +146,12 @@ public class LogFilePath {
 
     public String getLogFileParentDir() {
         ArrayList<String> elements = new ArrayList<String>();
-        elements.add(mPrefix);
-        elements.add(mTopic);
+        if(mPrefix.length() > 0) {
+            elements.add(mPrefix);
+        }
+        if(mTopic.length() > 0) {
+            elements.add(mTopic);
+        }
         return StringUtils.join(elements, "/");
     }
 
