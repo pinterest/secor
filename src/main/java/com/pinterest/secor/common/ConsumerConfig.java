df809202cfca7c23f3b7c79dca07375d067017bc
tygrash <ashwin.sinha@go-jek.com>
2017-05-13 22:10:49 +0530

    Heartbeat must be set lower than the session timeout

--- src/main/java/com/pinterest/secor/common/ConsumerConfig.java
+++ src/main/java/com/pinterest/secor/common/ConsumerConfig.java
@@ -21,7 +21,7 @@ public class ConsumerConfig {
         properties.put("max.poll.records", "1");
         properties.put("enable.auto.commit", "false");
         properties.put("session.timeout.ms", "100000");
-        properties.put("heartbeat.interval.ms", "100000");
+        properties.put("heartbeat.interval.ms", "90000");
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.BytesDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.BytesDeserializer");
     }
