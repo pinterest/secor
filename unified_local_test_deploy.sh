08447b61155d16bc3794bf61c87f2affd3634fb2
William Yu <wyu@unified.com>
2017-08-16 15:35:16 -0400

    UIP-1087 - Update readme.

--- unified_local_test_deploy.sh
+++ unified_local_test_deploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+[ -z "${SECOR_DEPLOY_DIR}" ] && declare SECOR_DEPLOY_DIR="secor_deploy"
+
 mvn package -DskipTests
 
-if [ -d secor_deploy ]; then
-    rm -rf secor_deploy
+if [ -d ${SECOR_DEPLOY_DIR} ]; then
+    echo "Removing old secor deployment: ${SECOR_DEPLOY_DIR}"
+    rm -rf ${SECOR_DEPLOY_DIR}
 fi
 
-mkdir secor_deploy
+echo "Creating secor deployment: ${SECOR_DEPLOY_DIR}"
+mkdir ${SECOR_DEPLOY_DIR}
 
 # TODO: Make the version # a regex
-tar -zxvf target/secor-0.23-SNAPSHOT-bin.tar.gz -C secor_deploy
-
-cd secor_deploy
+tar -zxvf target/secor-0.23-SNAPSHOT-bin.tar.gz -C ${SECOR_DEPLOY_DIR}
 
 # copy over test s3 client configuration profile over to deployment
-cp ../src/test/config/test.s3cfg .
+cp src/test/config/test.s3cfg ${SECOR_DEPLOY_DIR}
 
-sed -i '.bak' '/^host_bucket/ s/%(bucket)s/test-bucket/' test.s3cfg
+sed -i '.bak' '/^host_bucket/ s/%(bucket)s/test-bucket/' ${SECOR_DEPLOY_DIR}/test.s3cfg
 
 # set ENV variable to use local secor
 export SECOR_LOCAL_S3=true
