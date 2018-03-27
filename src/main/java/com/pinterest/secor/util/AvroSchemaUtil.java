15765f2e9ff72651cdefaf273a978bae2d86afc2
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-03-27 17:50:58 -0400

    Make schema subject suffix explicit and add to properties file

--- src/main/java/com/pinterest/secor/util/AvroSchemaUtil.java
+++ src/main/java/com/pinterest/secor/util/AvroSchemaUtil.java
@@ -5,10 +5,10 @@ import com.pinterest.secor.common.SecorConfig;
 public class AvroSchemaUtil {
 
     public static String getAvroSubjectSuffix(SecorConfig config) {
-        return config.getString("avro.schema.subject.suffix");
+        return config.getString("avro.schema.subject.suffix", "");
     }
 
     public static String getAvroSubjectOverride(SecorConfig config) {
-        return config.getString("avro.schema.subject.override");
+        return config.getString("avro.schema.subject.override", "");
     }
 }
