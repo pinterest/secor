3b392ddf1062773fc054fb3ec1b34825a29468ba
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-03-27 00:57:56 -0400

    Add avro schema config settings

---
+++ src/main/java/com/pinterest/secor/util/AvroSchemaUtil.java
@@ -0,0 +1,14 @@
+package com.pinterest.secor.util;
+
+import com.pinterest.secor.common.SecorConfig;
+
+public class AvroSchemaUtil {
+
+    public static String getAvroSubjectSuffix(SecorConfig config) {
+        return config.getString("avro.schema.subject.suffix", "-value");
+    }
+
+    public static String getAvroSubjectOverride(SecorConfig config) {
+        return config.getString("avro.schema.subject.override", "");
+    }
+}
