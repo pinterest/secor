dae0fc2639e525568e542564986214a227741d5b
Roy Sindre Norangshol <norangshol@zedge.net>
2018-05-28 12:57:48 +0200

    Remove dotDash ./ so we can use fully qualified paths

--- src/main/scripts/docker-entrypoint.sh
+++ src/main/scripts/docker-entrypoint.sh
@@ -130,6 +130,6 @@ cd /opt/secor
 DEFAULT_CLASSPATH="*:lib/*"
 CLASSPATH=${CLASSPATH:-$DEFAULT_CLASSPATH}
 
-java -Xmx${JVM_MEMORY:-512m} $JAVA_OPTS -ea -Dsecor_group=${SECOR_GROUP:-partition} -Dlog4j.configuration=file:./${LOG4J_CONFIGURATION:-log4j.docker.properties} \
+java -Xmx${JVM_MEMORY:-512m} $JAVA_OPTS -ea -Dsecor_group=${SECOR_GROUP:-partition} -Dlog4j.configuration=file:${LOG4J_CONFIGURATION:-log4j.docker.properties} \
         -Dconfig=${CONFIG_FILE:-secor.prod.partition.properties} $SECOR_CONFIG \
         -cp $CLASSPATH ${SECOR_MAIN_CLASS:-com.pinterest.secor.main.ConsumerMain}
