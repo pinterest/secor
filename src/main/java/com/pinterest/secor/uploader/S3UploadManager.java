05c4f08e7fb9cda622137090f05c8f5037a64916
clairefinnie <cfinnie@sailthru.com>
2020-08-04 13:48:07 +1200

    Adding canned ACL to uploadRequest.

--- src/main/java/com/pinterest/secor/uploader/S3UploadManager.java
+++ src/main/java/com/pinterest/secor/uploader/S3UploadManager.java
@@ -162,7 +162,7 @@ public class S3UploadManager extends UploadManager {
         }
 
         // make upload request, taking into account configured options for encryption
-        PutObjectRequest uploadRequest = new PutObjectRequest(s3Bucket, s3Key, localFile);
+        PutObjectRequest uploadRequest = new PutObjectRequest(s3Bucket, s3Key, localFile).withCannedAcl(CannedAccessControlList.BucketOwnerFullControl);
         if (!mConfig.getAwsSseType().isEmpty()) {
             if (S3.equals(mConfig.getAwsSseType())) {
                 LOG.info("uploading file {} to s3://{}/{} with S3-managed encryption", localFile, s3Bucket, s3Key);
