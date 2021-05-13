c7ac1eb7ebae9301686b71f38cc6d35387f24255
João Esteves <jvaesteves@gmail.com>
2021-05-13 15:09:52 -0300

    Add missing properties to KafkaProducer properties

--- src/main/java/com/pinterest/secor/common/KafkaProperties.java
+++ src/main/java/com/pinterest/secor/common/KafkaProperties.java
@@ -8,6 +8,7 @@ import java.util.function.Consumer;
 import com.pinterest.secor.util.IdUtil;
 
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 
 public class KafkaProperties {
 
@@ -20,8 +21,6 @@ public class KafkaProperties {
         
         props.put("bootstrap.servers", secorConfig.getKafkaSeedBrokerHost() + ":" + secorConfig.getKafkaSeedBrokerPort());
         props.put("client.id", IdUtil.getConsumerId());
-        props.put("key.deserializer", ByteArrayDeserializer.class);
-        props.put("value.deserializer", ByteArrayDeserializer.class);
         
         optionalConfig(secorConfig.getNewConsumerRequestTimeoutMs(), conf -> props.put("request.timeout.ms", conf));
         optionalConfig(secorConfig.getSocketReceiveBufferBytes(), conf -> props.put("receive.buffer.bytes", conf));
@@ -31,7 +30,7 @@ public class KafkaProperties {
     
     private static Properties getSecurityProperties(SecorConfig secorConfig) throws UnknownHostException {
         Properties props = new Properties();
-
+        
         optionalConfig(secorConfig.getSslKeyPassword(), conf -> props.put("ssl.key.password", conf));
         optionalConfig(secorConfig.getSslKeystoreLocation(), conf -> props.put("ssl.keystore.location", conf));
         optionalConfig(secorConfig.getSslKeystorePassword(), conf -> props.put("ssl.keystore.password", conf));
@@ -42,19 +41,19 @@ public class KafkaProperties {
         optionalConfig(secorConfig.getSslProtocol(), conf -> props.put("ssl.protocol", conf));
         optionalConfig(secorConfig.getSslProvider(), conf -> props.put("ssl.provider", conf));
         optionalConfig(secorConfig.getSslTruststoreType(), conf -> props.put("ssl.truststore.type", conf));
-
+        
         optionalConfig(secorConfig.getSaslClientCallbackHandlerClass(), conf -> props.put("sasl.client.callback.handler.class", conf));
         optionalConfig(secorConfig.getSaslJaasConfig(), conf -> props.put("sasl.jaas.config", conf));
         optionalConfig(secorConfig.getSaslKerberosServiceName(), conf -> props.put("sasl.kerberos.service.name", conf));
         optionalConfig(secorConfig.getSaslLoginCallbackHandlerClass(), conf -> props.put("sasl.login.callback.handler.class", conf));
         optionalConfig(secorConfig.getSaslLoginClass(), conf -> props.put("sasl.login.class", conf));
         optionalConfig(secorConfig.getSaslMechanism(), conf -> props.put("sasl.mechanism", conf));
-
+        
         optionalConfig(secorConfig.getSecurityProtocol(), conf -> props.put("security.protocol", conf));
-
+        
         return props;
     }
-
+    
     public static Properties getConsumerProperties(SecorConfig secorConfig) throws UnknownHostException {
         Properties props = getCommonProperties(secorConfig);
         props.putAll(getSecurityProperties(secorConfig));
@@ -62,6 +61,8 @@ public class KafkaProperties {
         props.put("group.id", secorConfig.getKafkaGroup());
         props.put("auto.offset.reset", secorConfig.getNewConsumerAutoOffsetReset());
         props.put("enable.auto.commit", false);
+        props.put("key.deserializer", ByteArrayDeserializer.class);
+        props.put("value.deserializer", ByteArrayDeserializer.class);
         
         optionalConfig(secorConfig.getMaxPollIntervalMs(), conf -> props.put("max.poll.interval.ms", conf));
         optionalConfig(secorConfig.getMaxPollRecords(), conf -> props.put("max.poll.records", conf));
@@ -72,10 +73,13 @@ public class KafkaProperties {
         
         return props;
     }
-
+    
     public static Properties getProducerProperties(SecorConfig secorConfig) throws UnknownHostException {
         Properties props = getCommonProperties(secorConfig);
         props.putAll(getSecurityProperties(secorConfig));
+
+        props.put("key.serializer", ByteArraySerializer.class);
+        props.put("value.serializer", ByteArraySerializer.class);
 
         return props;
     }
